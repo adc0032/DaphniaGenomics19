85ace7d1a443ad8684a252fe6fc9a0fda40bcc28
Bailey Howell <baileykhowell@gmail.com>
2019-05-31 13:10:22 -0500

    Added variables and filled in information to indices file, still user friendly.

--- GenomeOrg/Scripts/indices_uf.sh
+++ GenomeOrg/Scripts/indices_uf.sh
@@ -10,7 +10,7 @@
 ##send email abort; begin; end
 #PBS -m ae
 ##job name
-#PBS -N indices_{Organism}
+#PBS -N indices_Dpulex
 ##combine standard out and standard error
 #PBS -j oe
 # ----------------Load Modules-------------------- #
@@ -20,12 +20,32 @@ module load xz/5.2.2
 module load picard/2.4.1
 module load java/1.8.0_91
 #-----------------Define variables------------#
-WD="/scratch/userdirectory/"
-SD="/pathto/savedirectory"
-Seq="pathto/sequenceanalysis.fasta"
+WD="/scratch/bkh0024/"
+SD="/home/bkh0024/DaphniaGenomics19/GenomeOrg/ReferenceGenome"
+Seq="/home/bkh0024/DaphniaGenomics19/GenomeOrg/ReferenceGenome/PA42.fasta"
+cdate=`date|awk 'OFS="_"{print $2,$3}'
+ref="dpulex"
 # ----------------Commands------------------- #
 
+##Script is used to create indices reference genomes for bwa, samtools, and picardtools. Review 
+##Script make sure to replace relevant information with your own. 
+
+##Move to the working directory in scratch
 cd $WD
+
+##Makes variable directory from stripped sequence name (the part before the period only)
+dir=`basename $Seq|awk -F. '{print $1}'
+
+##Sets product directory variable to be named with stripped sequence name followed by date info
+pdir="$dir.indices_$cdate"
+
+##Test to see if product directory already exists, if not make it then cd into it, otherwise cd to it
+if [[ ! -d "$pdir" ]]; then
+	mkdir $pdir
+	cd $pdir
+else
+	cd $pdir
+fi
 
 ## command to create index reference genome
 bwa index $ref -a bwtsw $Seq
