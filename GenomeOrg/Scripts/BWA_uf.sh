145b679af94dbbf0b11a36d527145c5bb8d7468a
Amanda Clark <adc0032@auburn.edu>
2019-06-01 12:51:13 -0500

    updated BWA_uf.sh: added readgroup information in order to attach metadata to all reads for downstream analyses. checked that there were no split lanes, flowcells during sequencing. **Added a reference line that Bailey will need to fill in. (ref may need to be renamed to whatever prefix we chose for the indices, but will address later) Increased verbosity level to learn how the program is working updated variable usage in naming of bam file to use $sp instead of $dir Hashed out option for user input.

--- GenomeOrg/Scripts/BWA_uf.sh
+++ GenomeOrg/Scripts/BWA_uf.sh
@@ -23,19 +23,21 @@ SD="/home/bkh0024/DaphniaGenomics19/GenomeOrg/Results/"
 Seq1="/home/bkh0024/DaphniaGenomics19/GenomeOrg/Data/BA_411_USD16091408L_HKFJFDSXX_L3_1.fq"
 Seq2="/home/bkh0024/DaphniaGenomics19/GenomeOrg/Data/BA_411_USD16091408L_HKFJFDSXX_L3_2.fq"
 cdate=`date|awk 'OFS="_"{print $2,$3}'`
+ref="/path/to/ref/refname"
 # ----------------Commands------------------- #
 
 ###Script is used to run bwa on sequence files for mapping to reference. Review script, making sure to fill in your own
 ###variable information, and paying attention to your desired naming parameters. Assumes paired end data and will need two files.
-###User will want to run the fastqc script again to determine how trimming improved (or not) starting material
+###User may want to add readgroups to this step, but also (theoretially) can be done in picard tools.
+
+## to be used in a different setting. 
+#if [[ $# -lt 1 ]]; then
+#	echo "Script requires argument for reference name made in index script. This would be the prefix provided to indices_uf.sh (ex. qsub )"
+#else
+#	ref="$1"
 
 #move to working location in scratch; checks for/creates bwa product directory (pdir) to be zipped 
 #and returned to your home directory (SD)
-
-if [[ $# -lt 1 ]]; then
-	echo "Script requires argument for reference name made in index script."
-else
-	ref="$1"
 
 cd $WD
 dir=`basename $Seq|awk -F. '{print $1}'`
@@ -54,8 +56,8 @@ else
 ## -R requires readgroups. ID, PU and LB all should be unique if reads were split across lanes-especially.
 ## samtools steps following the pipe : -Sb converts SAM to BAM, sorts, outputs sorted bam files
 
-bwa mem -M  -v 2 -t 10 -R "@RG\tID:uniqueID\tSM:samplegroup\tPL:illumina\tPU:uniquereadid\tLB:uniquelibraryname" $ref \
-$Seq | samtools view -Sb | samtools sort > $dir.sorted.bam;
+bwa mem -M  -v 3 -t 10 -R "@RG\tID:HKFJFDSXX3\tSM:BA411\tPL:illumina\tPU:HKFJFDSXX8L3\tLB:USD16091408L" $ref \
+$Seq | samtools view -Sb | samtools sort > $sp.sorted.bam;
 
 #Product/output compression and relocation
 
