17eec971ecac2f2b0fa05a7bae54ae9668f2c25f
Bailey Howell <baileykhowell@gmail.com>
2019-06-10 10:07:58 -0500

    Fixed error with $Seq variable (changed it to $Seq1)

--- GenomeOrg/Scripts/BWA_uf.sh
+++ GenomeOrg/Scripts/BWA_uf.sh
@@ -40,7 +40,7 @@ ref="/home/bkh0024/DaphniaGenomics19/GenomeOrg/ReferenceGenome/PA42.indices_June
 #and returned to your home directory (SD)
 
 cd $WD
-dir=`basename $Seq|awk -F. '{print $1}'`
+dir=`basename $Seq1|awk -F. '{print $1}'`
 sp=`echo $dir|awk -F_ 'OFS="_"{print $1,$2}'`
 pdir="$sp.bwa_$cdate"
 
@@ -56,7 +56,7 @@ else
 ## -R requires readgroups. ID, PU and LB all should be unique if reads were split across lanes-especially.
 ## samtools steps following the pipe : -Sb converts SAM to BAM, sorts, outputs sorted bam files
 
-bwa mem -M  -v 3 -t 4 -R "@RG\tID:HKFJFDSXX3\tSM:BA411\tPL:illumina\tPU:HKFJFDSXX8L3\tLB:USD16091408L" $ref $Seq $Seq2 | samtools view -Sb | samtools sort > $sp.sorted.bam;
+bwa mem -M  -v 3 -t 4 -R "@RG\tID:HKFJFDSXX3\tSM:BA411\tPL:illumina\tPU:HKFJFDSXX8L3\tLB:USD16091408L" $ref $Seq1 $Seq2 | samtools view -Sb | samtools sort > $sp.sorted.bam;
 
 #Product/output compression and relocation
 
