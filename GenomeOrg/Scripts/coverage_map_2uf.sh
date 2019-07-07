acad5b67612bf8e46b1891185e87e6e7ddce1ba6
Bailey Howell <baileykhowell@gmail.com>
2019-07-07 15:27:21 -0500

    Added the scripts I used to determine whether we had coverage for each strain at a particular position with respect to the reference. Originally this script contained a separate for loop for the three commands performed, but I consolidated it for more streamlined running.

---
+++ GenomeOrg/Scripts/coverage_map_2uf.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# ----------------QSUB Parameters----------------- #
+##choose queue
+####PBS -q
+##list - node are nodes: ppn are cpus per node: walltime=walltime
+#PBS -l nodes=1:ppn=4,mem=16gb,walltime=120:00:00
+##email
+#PBS -M baileykhowell@gmail.com
+##send email abort; begin; end
+#PBS -m ae
+##job name
+#PBS -N scaffolds_DpulicariaBA 
+##combine standard out and standard error
+#PBS -j oe
+# ----------------Load Modules-------------------- #
+
+#-----------------Define variables------------#
+WD="/scratch/bkh0024"
+cdate=`date|awk 'OFS="_"{print $2,$3}'`
+SD="/home/bkh0024/DaphniaGenomics19/GenomeOrg/ReferenceGenome/ChromosomeOrg/IndividualChrFiles"
+Seq1="/home/bkh0024/DaphniaGenomics19/GenomeOrg/Data/BA_411_USD16091408L_HKFJFDSXX_L3_1.fq"
+# ----------------Commands------------------- #
+##change directories to working directory
+cd $WD
+
+##create variables to name product directory then change directories to product directory
+dir=`basename $Seq1|awk -F. '{print $1}'`
+sp=`echo $dir|awk -F_ 'OFS="_"{print $1,$2}'`
+pdir="$sp.scaffolds_$cdate"
+
+if [[ ! -d "$pdir" ]]; then
+        mkdir $pdir
+        cd $pdir
+else
+        cd $pdir
+fi
+
+##create variable for our depth file
+depth="/home/bkh0024/DaphniaGenomics19/GenomeOrg/Results/$sp.qc2_Jun_27/$sp.DC.txt"
+
+##commands to output the scaffolds that correspond each chromosome from supplemental material, then search for these scaffolds in our depth file, then make a file
+##that shows whether we have coverage >5x at a given position (0 means no, 1 means yes)
+
+for num in {1..12}
+do
+        awk '{print $1}' $SD/Chr.$num.Org.txt | sort | uniq > Scaffolds.Chr.$num.txt
+        grep -F -f Scaffolds.Chr.$num.txt $depth > $sp.Depth.Scaff.Chr.$num.txt
+        awk 'BEGIN {OFS="\t"} { if ($3 < 5) {print $1,$2,"0"} else {print $1,$2,"1"} }' $sp.Depth.Scaff.Chr.$num.txt > $sp.Cov_Chr_$num.txt
+done
+
+##commands to move up a directory, tar pdir and move it to our save directory
+cd ..
+
+tar -cvf $pdir.tar $pdir;
+
+mv $pdir.tar $SD
